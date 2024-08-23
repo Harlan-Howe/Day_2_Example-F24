7306a8bc6b6e7790f28b2fbadb40ce0bd5038af2
Harlan Howe <harlan.howe@kinkaid.org>
2024-08-23 11:08:25 -0500

    Now makes a PracticeClass and calls some methods.

--- src/Day2Runner.java
+++ src/Day2Runner.java
@@ -3,5 +3,8 @@ public class Day2Runner
     public void main()
     {
         System.out.println("Running.");
+        PracticeClass theProgram = new PracticeClass();
+        theProgram.greetUser();
+        theProgram.sayHello5Times();
     }
 }
