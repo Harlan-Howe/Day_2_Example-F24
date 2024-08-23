2d720c5522021694682365db86ba88fbe142988c
Harlan Howe <harlan.howe@kinkaid.org>
2024-08-23 11:08:55 -0500

    Wrote constructor, greetUser() and sayHello5Times() methods.

--- src/PracticeClass.java
+++ src/PracticeClass.java
@@ -1,4 +1,34 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class PracticeClass
 {
+    private String myName;
+    private Scanner keyReader;
 
+    public PracticeClass() // constructor
+    {
+        System.out.println("Making a practice class.");
+        keyReader = new Scanner(System.in);
+    }
+
+    public void greetUser()
+    {
+        System.out.println("What is your name? ");
+
+        myName = keyReader.nextLine();
+
+        System.out.println(STR."Hello, \{myName}!!!");
+    }
+
+    public void sayHello5Times()
+    {
+        int x = 0;
+        while (x<5)
+        {
+            System.out.println(STR."Howdy, \{myName}.");
+            x = x + 1;
+        }
+    }
 }
